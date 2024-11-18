76b80de191d0f31904ffeb4e6ac317b235f7ca5c
yehonatna <yonidil1997@gmail.com>
2024-11-18 13:06:59 +0200

    add Q3

--- Assets/beatting.cs
+++ Assets/beatting.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 
-public class beatting : MonoBehaviour
+public class Beatting : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("The maximum size of the object")]
+    float maxsize = 10f;
+    [SerializeField]
+    [Tooltip("The minimum size of the object")]
+    float minsize = 1f;
+    [SerializeField]
+    [Tooltip("Scaling speed")]
+    float speed = 1f;
+    float size;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +22,9 @@ public class beatting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         float t = (Mathf.Sin(Time.time * speed) + 1f) * 0.5f;
+         size = Mathf.Lerp(minsize, maxsize, t);
+         transform.localScale = new Vector3(size, size, size);
     }
+    
 }
